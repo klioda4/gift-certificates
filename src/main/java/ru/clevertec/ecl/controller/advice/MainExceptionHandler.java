dc855409701b9587ea4a8a7d3881e00107b25cdb
Klioda4 <klio.4815162342@gmail.com>
2022-10-16 17:51:53 +0300

    - Handle exceptions of invalid requests - Add not empty constraint to tags when add new certificate

--- src/main/java/ru/clevertec/ecl/controller/advice/MainExceptionHandler.java
+++ src/main/java/ru/clevertec/ecl/controller/advice/MainExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.clevertec.ecl.controller.advice;
 
+import javax.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.dao.DataAccessException;
@@ -9,13 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ru.clevertec.ecl.dto.response.ExceptionResponse;
 import ru.clevertec.ecl.exception.EntityNotFoundException;
 import ru.clevertec.ecl.exception.IntegrityViolationException;
-import ru.clevertec.ecl.exception.ValidationException;
 import ru.clevertec.ecl.util.constant.ErrorDescription;
 
 @Slf4j
@@ -38,12 +40,6 @@ public class MainExceptionHandler {
         return getResponseEntity(HttpStatus.NOT_FOUND, ErrorDescription.OBJECT_NOT_EXISTS.getCode(), e.getMessage());
     }
 
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<ExceptionResponse> handleValidationException(Exception e) {
-        log.info(e.getMessage());
-        return getResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY, -1, e.getMessage());
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.info(e.getMessage());
@@ -56,9 +52,27 @@ public class MainExceptionHandler {
                 messageBuilder.append(" - ");
                 messageBuilder.append(fieldError.getDefaultMessage());
             });
+        String answerMessage = messageBuilder.toString();
+        log.info("Message to answer: {}", answerMessage);
         return getResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY,
                                  ErrorDescription.DTO_VALIDATION_FAILED.getCode(),
-                                 messageBuilder.toString());
+                                 answerMessage);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionResponse> handleConstraintViolationException(ConstraintViolationException e) {
+        log.info(e.getMessage());
+        return getResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY,
+                                 ErrorDescription.ARGUMENT_VALIDATION_FAILED.getCode(),
+                                 e.getMessage());
+    }
+
+    @ExceptionHandler({MissingServletRequestParameterException.class, MethodArgumentTypeMismatchException.class})
+    public ResponseEntity<ExceptionResponse> handleExceptionOfIncorrectOrMissingArgument(Exception e) {
+        log.info(e.getMessage());
+        return getResponseEntity(HttpStatus.BAD_REQUEST,
+                                 ErrorDescription.INCORRECT_OR_MISSING_ARGUMENT.getCode(),
+                                 e.getMessage());
     }
 
     @ExceptionHandler(DataAccessException.class)
--- src/main/java/ru/clevertec/ecl/util/constant/ErrorDescription.java
+++ src/main/java/ru/clevertec/ecl/util/constant/ErrorDescription.java
@@ -7,11 +7,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorDescription {
 
+    INCORRECT_OR_MISSING_ARGUMENT(40001, ""),
     OBJECT_NOT_EXISTS(40401, "Requested object not found"),
     METHOD_NOT_SUPPORTED(40501, ""),
     DATABASE_UPDATE_FAILED(40901, "Can't update data without integrity breach, check the request or try later"),
     TAG_ALREADY_EXISTS(40902, "Tag with specified name already exists"),
     DTO_VALIDATION_FAILED(42201, ""),
+    ARGUMENT_VALIDATION_FAILED(42202, ""),
     UNHANDLED_SERVER_ERROR(50001, "Unhandled server error, try to request later or contact support"),
     DATABASE_ERROR(50002, "Database problem, try to request later or contact support");
 
